8030ab8bc91b4f47d7dcbb4c96ab5424c4e18607
nancyhe1 <nancyhe098@gmail.com>
2024-11-22 20:59:30 -0500

    Added face mesh

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -7,26 +7,44 @@
  */
 
 "use strict";
+let faceMesh;
+let video;
+let faces = [];
+let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
 
-/**
- * Description of preload
-*/
 function preload() {
-
+  // Load the faceMesh model
+  faceMesh = ml5.faceMesh(options);
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
-
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  // Start detecting faces from the webcam video
+  faceMesh.detectStart(video, gotFaces);
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
 
+  // Draw all the tracked face points
+  for (let i = 0; i < faces.length; i++) {
+    let face = faces[i];
+    for (let j = 0; j < face.keypoints.length; j++) {
+      let keypoint = face.keypoints[j];
+      fill(0, 255, 0);
+      noStroke();
+      circle(keypoint.x, keypoint.y, 5);
+    }
+  }
+}
+
+// Callback function for when faceMesh outputs data
+function gotFaces(results) {
+  // Save the output to the faces variable
+  faces = results;
 }
